6775f9d4ac71245a0aa1e66a67d2fd11806a3f82
Ivan Pico <ipico@mediktor.com>
2016-11-01 23:58:29 +0100

    Paginated 5 items

--- app/src/main/java/network/TrakTvApiEndpointInterface.java
+++ app/src/main/java/network/TrakTvApiEndpointInterface.java
@@ -5,6 +5,8 @@ import models.Trend;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by ivan.pico.martin
@@ -17,6 +19,14 @@ public interface TrakTvApiEndpointInterface {
 
     @GET("/movies/trending")
     Call<Trend[]> getTrending();
+
+    @Headers({  "trakt-api-key: 019a13b1881ae971f91295efc7fdecfa48b32c2a69fe6dd03180ff59289452b8",
+            "Content-type: application/json",
+            "trakt-api-version: 2"})
+    @GET("/movies/trending")
+    Call<Trend[]> getTrendingPaginated(@Query("page") int page, @Query("limit") int limit);
+
+
 
 
 }
